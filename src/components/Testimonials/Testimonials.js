aa8344965539a5072d482bd8802533017375957b
Ashish <bansalgashi083@gmail.com>
2022-12-29 23:57:47 +0530

    review section complete

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import css from './Testimonials.module.css'
 import Hero from '../../assets/testimonialHero.png'
+// import Swiper from 'swiper'
+import {TestimonialsData} from '../../data/testimonials'
+import { Swiper,SwiperSlide } from 'swiper/react'
 
 const Testimonials = () => {
   return (
@@ -15,6 +18,29 @@ const Testimonials = () => {
                 <span>100k</span>
                 <span>Happy customers with us</span>
             </div>
+        </div>
+        <div className={css.reviews}>Reviews</div>
+        <div className={css.carousal}>
+            <Swiper
+            slidesPerView={3}
+            slidesPerGroup={1}
+            spaceBetween={20}
+            className={css.tCarousal}
+            >
+                {
+                    TestimonialsData.map((testimonial,i)=>(
+                        <SwiperSlide>
+                            <div className={css.testimonial }>
+                                <img src={testimonial.image} alt='img'/>
+                                <span>{testimonial.comment}</span>
+                                <hr/>
+                                <span>{testimonial.name}</span>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+
         </div>
     </div>
   )
