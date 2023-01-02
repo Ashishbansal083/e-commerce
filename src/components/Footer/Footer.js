d227fbb87fe01e6a32b1b0fd34233f43342d72b6
Ashish <bansalgashi083@gmail.com>
2023-01-02 22:38:00 +0530

    footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import css from './Footer.module.css'
 import logo from '../../assets/logo.png'
-import {
-    InboxIcon,
-    PhoneIcon,
-    LocationMarkerIcon,
-    LoginIcon,
-    UsersIcon,
-    LinkIcon,
+// import {
+//     InboxIcon,
+//     PhoneIcon,
+//     LocationMarkerIcon,
+//     LoginIcon,
+//     UsersIcon,
+//     LinkIcon,
+//     MappinIcon,
+//     AcademicCapIcon
+    
+    
 
-} from '@heroicons/react/outline' ;
+// } from 'heroicons-react/24/outline' ;
+import {HiOutlineLocationMarker,HiUser,HiOutlineLogin,HiOutlinePhone,HiInbox} from 'react-icons/hi';
+
 
 const Footer = () => {
   return (
@@ -19,6 +25,47 @@ const Footer = () => {
             <div className={css.logo}>
             <img src={logo} alt=""/>
             <span>amazon</span>
+            </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Contact us</span>
+                <span className={css.pngline}>
+                  <HiOutlineLocationMarker className={css.icon}/> 
+                  <span>11 north anue orlando,FL 32801</span>
+                </span>
+                <span className={css.pngline}>
+                  <HiOutlinePhone className={css.icon}/> 
+                  <span>352-306-4415</span>
+                </span>
+                <span className={css.pngline}>
+                  <HiInbox className={css.icon}/> 
+                  <span>support</span>
+                </span>
+
+              </div>
+
+            </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Account</span>
+                <span className={css.pngline}>
+                  <HiOutlineLogin className={css.icon}/> 
+                  <span>Sign in</span>
+                </span>
+                
+              </div>
+
+            </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Company</span>
+                <span className={css.pngline}>
+                  <HiUser className={css.icon}/> 
+                  <span>About us</span>
+                </span>
+                
+              </div>
+
             </div>
         </div>
 
